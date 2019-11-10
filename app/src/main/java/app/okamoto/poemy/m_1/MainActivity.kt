16fe3a203ca27be689510baf5fc4f1a0b0ebd22d
poemy_Okamoto <57334338+poemy2810@users.noreply.github.com>
2019-11-10 22:46:12 +0900

    Initial commit

---
+++ app/src/main/java/app/okamoto/poemy/m_1/MainActivity.kt
@@ -0,0 +1,25 @@
+package app.okamoto.poemy.m_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myImageView.visibility = View.INVISIBLE
+        myText.visibility = View.INVISIBLE
+
+        myNameButton.setOnClickListener {
+
+            myImageView.visibility = View.VISIBLE
+            myText.visibility = View.VISIBLE
+        }
+    }
+
+
+}
